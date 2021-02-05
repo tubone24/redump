9499866b46f097cd1b76b162e34db89bbf104ce9
tubone24 <tubo.yyyuuu@gmail.com>
2021-02-06 01:44:14 +0900

    add test

--- pkg/utils/file_test.go
+++ pkg/utils/file_test.go
@@ -14,6 +14,21 @@ func TestGetContentType(t *testing.T) {
 		expected string
 	}{
 		{"test.txt", "text/plain"},
+		{"test.htm", "text/html"},
+		{"test.html", "text/html"},
+		{"test.css", "text/css"},
+		{"test.js", "text/javascript"},
+		{"test.mjs", "text/javascript"},
+		{"test.png", "image/png"},
+		{"test.jpg", "image/jpeg"},
+		{"test.jpeg", "image/jpeg"},
+		{"test.gif", "image/gif"},
+		{"test.bmp", "image/bmp"},
+		{"test.svg", "image/svg+xml"},
+		{"test.mp3", "audio/mpeg"},
+		{"test.mp4", "video/mp4"},
+		{"test.mpg", "video/mpeg"},
+		{"test.mpeg", "video/mpeg"},
 		{"test.tsv", "text/tab-separated-values"},
 		{"test.csv", "text/csv"},
 		{"test.json", "application/json"},
@@ -36,6 +51,36 @@ func TestGetContentType(t *testing.T) {
 		{"test.7z", "application/x-7z-compressed"},
 		{"test.xml", "application/xml"},
 		{"test.bin", "application/octet-stream"},
+		{"test.class", "application/octet-stream"},
+		{"test.dll", "application/octet-stream"},
+		{"test.dmg", "application/octet-stream"},
+		{"test.dms", "application/octet-stream"},
+		{"test.dpc", "application/octet-stream"},
+		{"test.dpt", "application/octet-stream"},
+		{"test.dpv", "application/octet-stream"},
+		{"test.exe", "application/octet-stream"},
+		{"test.mvr", "application/octet-stream"},
+		{"test.so", "application/octet-stream"},
+		{"test.wpi", "application/octet-stream"},
+		{"test.cab", "application/octet-stream"},
+		{"test.aac", "audio/aac"},
+		{"test.abw", "application/x-abiword"},
+		{"test.arc", "application/x-freearc"},
+		{"test.avi", "video/x-msvideo"},
+		{"test.azw", "application/vnd.amazon.ebook"},
+		{"test.csh", "application/x-csh"},
+		{"test.eot", "application/vnd.ms-fontobject"},
+		{"test.epub", "application/epub+zip"},
+		{"test.ico", "image/vnd.microsoft.icon"},
+		{"test.ics", "text/calendar"},
+		{"test.jsonld", "application/ld+json"},
+		{"test.mid", "audio/midi"},
+		{"test.midi", "audio/midi"},
+		{"test.mpkg", "application/vnd.apple.installer+xml"},
+		{"test.odp", "application/vnd.oasis.opendocument.presentation"},
+		{"test.ods", "application/vnd.oasis.opendocument.spreadsheet"},
+		{"test.odt", "application/vnd.oasis.opendocument.text"},
+		{"test.oga", "audio/ogg"},
 		{"test.mse", "application/x-www-form-urlencoded"},
 	}
 
@@ -75,5 +120,12 @@ func TestReadWriteFile(t *testing.T) {
 	if actualJson.Test != testText {
 		t.Errorf("expected: %s, actual %s", testText, actualJson.Test)
 	}
+}
 
+func TestSanitizeInvalidFileName(t *testing.T) {
+	testStr := "test test　testtest.com"
+	actual := utils.SanitizeInvalidFileName(testStr)
+	if actual != "test_test_testtest.com" {
+		t.Errorf("expected: %s, actual %s", "test_test_testtest.com", testStr)
+	}
 }
