97668c619b63d1b57906d8dff3ef96f0658d67bf
tubone24 <tubo.yyyuuu@gmail.com>
2021-02-07 21:50:21 +0900

    :recycle: Benchmark test improve

--- pkg/utils/struct_test.go
+++ pkg/utils/struct_test.go
@@ -3,6 +3,7 @@ package utils_test
 import (
 	"github.com/tubone24/redump/pkg/redmine"
 	"github.com/tubone24/redump/pkg/utils"
+	"strconv"
 	"testing"
 )
 
@@ -79,6 +80,63 @@ func TestDeepCopyInvalidDst(t *testing.T) {
 	if err == nil {
 		t.Error("Error does not occurred")
 	}
+}
+
+func BenchmarkDeepCopy(b *testing.B) {
+	var dest redmine.Issues
+	issueJsons100 := make(redmine.Issues, 100)
+	for i := 0; i <100; i++ {
+		ij := redmine.Issue{
+			Id:      1,
+			Project: redmine.Project{Id: 1, Name: "testProject"},
+			Tracker: redmine.Tracker{Id: 1, Name: "doing"},
+			Status:  redmine.Status{Id: 1, Name: "test"}, Priority: redmine.Priority{Id: 1, Name: "High"},
+			Author:      redmine.Author{Id: 1, Name: "testUser"},
+			AssignedTo:  redmine.AssignedTo{Id: 1, Name: "testUser"},
+			Subject:     "test1" + strconv.Itoa(i),
+			Description: "testtesttesttest" + strconv.Itoa(i),
+			StartDate:   "2020-01-01T00:00:00Z",
+			CustomFields: redmine.CustomFields{&redmine.CustomField{
+				Id:       1,
+				Name:     "customField1",
+				Multiple: true,
+				Value:    []string{"aaaa", "bbb", "ccc"}}},
+			CreatedOn: "2020-01-01T00:00:00Z",
+			UpdatedOn: "2020-01-01T00:00:00Z",
+			Attachments: redmine.Attachments{&redmine.Attachment{
+				Id: 1, FileName: "test.png",
+				FileSize:    12000,
+				ContentUrl:  "http://example.com/test.png",
+				Description: "testFile",
+				Author:      redmine.Author{Id: 1, Name: "testUser"},
+				CreatedOn:   "2020-01-01T00:00:00Z"}},
+			Journals: redmine.Journals{&redmine.Journal{
+				Id:        1,
+				User:      redmine.User{Id: 1, Name: "testUser"},
+				Notes:     "testtest",
+				CreatedOn: "2020-01-01T00:00:00Z"},
+				&redmine.Journal{
+					Id:        2,
+					User:      redmine.User{Id: 1, Name: "testUser"},
+					Notes:     "testtest2",
+					CreatedOn: "2020-01-01T00:00:00Z"},
+				&redmine.Journal{
+					Id:        3,
+					User:      redmine.User{Id: 1, Name: "testUser"},
+					Notes:     "testtest",
+					CreatedOn: "2020-01-01T00:00:00Z", Details: redmine.Details{&redmine.Detail{
+						Property: "change",
+						Name:     "upload",
+						OldValue: "aaa",
+						NewValue: "bbb"}}},
+			},
+			Watchers: redmine.Watchers{&redmine.Watcher{
+				Id: 1, Name: "testUser"}, &redmine.Watcher{Id: 2, Name: "testUser2"}, &redmine.Watcher{Id: 3, Name: "testUser3"}},
+		}
+		issueJsons100[i] = &ij
+	}
+	b.ResetTimer()
+	_ = utils.DeepCopy(&dest, &issueJsons100)
 }
 
 func ExampleDeepCopy() {
