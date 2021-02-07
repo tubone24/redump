41bb5197043ab306eb741263aa645fb7dea1f8cf
tubone24 <tubo.yyyuuu@gmail.com>
2021-02-07 20:35:22 +0900

    :recycle: Fix project directory layout
    Because cmd package is not used by other project

--- cmd/redump/main.go
+++ cmd/redump/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docopt/docopt-go"
-	"github.com/tubone24/redump/pkg/cmd"
+	"github.com/tubone24/redump/internal/cmd"
 	"github.com/tubone24/redump/pkg/config"
 )
 
